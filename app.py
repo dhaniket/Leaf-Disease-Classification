4204f3a58c1515993fbcff02baf49de315199b66
Dhaniket <dhaniketshambharkar12@gmail.com>
2022-09-10 11:21:08 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+import tensorflow as tf
+st.title("Leaf Disease Classification")
+
+file = st.file_uploader("Upload Image", type=["jpg", "png"])
+
+model = tf.keras.models.load_model('myModel.hdf5')
+
+CLASS_NAMES = ["Early Blight","Late Blight","Healthy"]
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    nimg = np.array(image)
+    img = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
+    img_array = tf.expand_dims(img,0)
+    st.image(image, use_column_width=True)
+    predictions = model.predict(img_array)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    st.write(predicted_class)
+    st.write(confidence)
